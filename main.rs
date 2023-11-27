9eca2d2fedf814acd645b72a12ecfb0991443bb4
ryuichi1208 <ryucrosskey@gmail.com>
2023-11-28 00:22:43 +0900

    fist commit

--- main.rs
+++ main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+
+    let mut x;
+    x = 6;
+    println!("The value of x is: {}", x);
+    x = 2;
+    println!("The value of x is: {}", x);
+
+    let x:i32 = 6;
+    println!("The value of x is: {}", x);
+
+    let y:u32 = 6;
+    println!("The value of x is: {}", y);
+
+    println!("The value of x is: {}", add(2, 3));
+}
+
+fn add(x:i32, y:i32) -> i32 {
+    x + y
 }
